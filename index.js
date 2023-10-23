05eca9ba47a97929ba89f19bcbdfca0e61d5941c
f3l1p397 <juanfehidalgo@gmail.com>
2023-10-23 16:52:49 -0500

    POST: metodo para crear

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const routerApi = require('./routes');
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
@@ -12,32 +13,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  res.send('Yo soy un filter');
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Produc 2 ',
-    price: 2000,
-  });
-});
+routerApi(app);
 
 app.listen(port, () => {
   console.log('Mi port' + port);
---
+++ routes/users.router.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.person.firstName(),
+      sexo: faker.person.sex(),
+      number: faker.phone.number(),
+    });
+  }
+  res.json(products);
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Nombre de persona',
+    edad: 20,
+  });
+});
+
+module.exports = router;
