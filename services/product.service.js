e3c5746e866c0c143e4e77796c3a4dcad87a41f9
f3l1p397 <juanfehidalgo@gmail.com>
2023-10-25 17:07:06 -0500

    Async await y captura de errores

--- services/product.service.js
+++ services/product.service.js
@@ -17,7 +17,7 @@ class ProductsServices {
     }
   }
 
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: faker.string.uuid(),
       ...data,
@@ -27,14 +27,18 @@ class ProductsServices {
   }
 
   find() {
-    return this.products;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    });
   }
 
-  findOne(id) {
+  async findOne(id) {
     return this.products.find((item) => item.id === id);
   }
 
-  update(id, changes) {
+  async update(id, changes) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product not found');
@@ -43,7 +47,7 @@ class ProductsServices {
     this.products[index] = { ...product, ...changes };
     return this.products[index];
   }
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product not found');
